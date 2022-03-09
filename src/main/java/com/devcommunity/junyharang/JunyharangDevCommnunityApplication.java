6d578eb2aec997da138babde3ed5978e75718247
junyharang <junyharang8592@gmail.com>
2022-03-09 16:06:23 +0900

    [개발 지원 - Q&A 게시판] VO 생성 및 Paging 처리용 Class 구현 완료

---
+++ src/main/java/com/devcommunity/junyharang/JunyharangDevCommnunityApplication.java
@@ -0,0 +1,13 @@
+package com.devcommunity.junyharang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunyharangDevCommnunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunyharangDevCommnunityApplication.class, args);
+    }
+
+}
